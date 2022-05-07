ec57aff4b5b9dc778cdfcffb47e6fa41758b6e0c
Guilhermee19 <guilherme.santana1998@gmail.com>
2022-05-07 12:57:33 -0300

    ajuste

--- src/components/BannerTop/BannerTop.jsx
+++ src/components/BannerTop/BannerTop.jsx
@@ -1,23 +1,38 @@
+import React, { useEffect } from "react";
 import './style.css';
 
 function BannerTop({ banner }) {
+    
+    useEffect(() => {
+        viewBanner()
+        console.log(banner);
+      }, [banner])
 
-    return (
-        <div className='card_slide'>
-            <img src={`https://www.themoviedb.org/t/p/original/${banner.backdrop_path}`} className='image_poster' />
+      let version = ['w533_and_h300_bestv2','w600_and_h900_bestv2','original']
 
-            <div className='effect_glass'>
-                <div className='card_info'>
-                    <h2> {banner.title != null ? banner.title : banner.name} </h2>
-                    <h3> {banner.overview } </h3>
-                    {/* <button className='btn_details'> Mais Detalhes </button> */}
-                </div>
-            </div>
+      function viewBanner() {
+          return (
+              <div className='card_slide'>
+                  <img src={`https://www.themoviedb.org/t/p/${version[2]}/${banner.backdrop_path}`} className='image_poster' />
+      
+                  <div className='effect_glass'>
+                      <div className='card_info'>
+                          <h2> {banner.title != null ? banner.title : banner.name} </h2>
+                          <h3> {banner.overview } </h3>
+                          {/* <button className='btn_details'> Mais Detalhes </button> */}
+                      </div>
+                  </div>
+      
+                  <img src={`https://www.themoviedb.org/t/p/${version[0]}/${banner.backdrop_path}`} className='image_background' />
+      
+              </div>
+          )
+      }
 
-            <img src={`https://www.themoviedb.org/t/p/original/${banner.backdrop_path}`} className='image_background' />
-
-        </div>
+      return (
+        viewBanner()
     )
+      
 }
 
 export default BannerTop;
